7ce3e3f5c83bb69699d1a0f2deaef7dcee3a07d3
a.brocke@fme-us.com <a.brocke@fme-us.com>
2019-11-26 11:08:36 -0500

    separated encryption classes

--- src/com/seshion/Server.java
+++ src/com/seshion/Server.java
@@ -5,95 +5,115 @@ import java.nio.ByteBuffer;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.spec.MGF1ParameterSpec;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
 import javax.crypto.spec.SecretKeySpec;
 
 import java.io.*;
 
 public class Server {
 
-	/* initialize socket and input stream */
+	/* declare socket, streams, and keys */
 	private Socket socket = null;
 	private ServerSocket server = null;
-	private DataInputStream inStream = null;
-	private DataOutputStream outStream = null;
+	private DataInputStream dataBufferedNetInputStream = null;
+	private DataOutputStream dataBufferedNetOutputStream = null;
+	private DataOutputStream dataNetOutputStream = null;
+	/* RSA */
 	private String publicKey = null;
 	private String privateKey = null;
+	/* AES */
 	private String aesSymmetricKey = null;
 	
 	/* constructor with parameter port number */
 	public Server(int port) {
-		/* start server and wait for a connection */
+		
 		try
 		{
+			/* start server and wait for a connection */
 			server = new ServerSocket(port);
 			System.out.println("Server started");
 			System.out.println("waiting for a client to connect on port " + port + "...");
 			
+			/* accept connection */
 			socket = server.accept();
 			System.out.println("Client accepted!");
 			
-			/* make our streams which send and receive the data */
-			outStream = new DataOutputStream(
-					new BufferedOutputStream(
-						socket.getOutputStream()
-						));
-					
-			inStream = new DataInputStream(
-					new BufferedInputStream(
-							socket.getInputStream())
-					);
+			/* netOutputStream decorated by bufferedInputStream, decorated by dataOutputStream */
+			dataBufferedNetOutputStream = new DataOutputStream( new BufferedOutputStream( socket.getOutputStream() ) );
 			
+			/* without the buffered to send simple data types */
+			dataNetOutputStream = new DataOutputStream ( socket.getOutputStream() );
 			
+			/* netInputStream decorated by bufferedInputStream, decorated by dataInputStream */
+			dataBufferedNetInputStream = new DataInputStream( new BufferedInputStream( socket.getInputStream() ) );
 			
-			byte[] key = new byte[16];
+			System.out.println("Making RSA Keys...");
+			/* generate RSA public and private keys */
+			RSA rsa = null;
 			
-			/* reads message from client until End of file exception
-			 * is encountered. This is actually how the DataInputStream.readUTF
-			 * is told that there is no more data. */
+			/* generate keys of given length */
+			rsa = new RSA();
+			rsa.generateKeys(1024);
+			System.out.println("keys created!");
+			
+			/* fetch public key from RSA object */
+			byte[] publicKey = rsa.getPublicKey().getEncoded();
+			
+			/* send key length and public key to client */			
+			int publicKeyLength = publicKey.length;
+			System.out.println("public key length:" + publicKeyLength + "\nAttempting to send public key length to client...");
+			dataNetOutputStream.writeInt(publicKeyLength);
+			System.out.println("Attempting to send public key to client...");
+			dataNetOutputStream.write(publicKey,  0, publicKeyLength);
+			System.out.println("Sent!");
 
-			//while (true) {
-				
-				try {
-					
-					/* obtain AES (symmetric) key information from client */
-					inStream.readFully(key, 0, 16);
-					System.out.println("obtained key from client:" + key);
-					
-					
-					
-				}catch(EOFException eofe) {
-					System.out.println("End Of File Exception, no more data sent in this stream\n" + eofe);
-					//eofe.printStackTrace();
-					//break;
-					
-				}catch(IOException ioe) {
-					System.out.println("IO encountered while server running:\n" + ioe);
-					ioe.printStackTrace();
-				}
-				
-			//}
-			
+			/* making symmetric key 16 bytes, (client knows to send one that length) */
+			byte[] encryptedAESKey = new byte[128];
+			byte[] realAESKey = new byte[16];
 
+			/* obtain encrypted AES (symmetric) key information from client */	
+			try {
+				
+				/* after RSA encryption, total size of sent data is 128 */
+				dataBufferedNetInputStream.readFully(encryptedAESKey, 0, 128);
+				System.out.println("obtained key from client:" + encryptedAESKey);
+				
+			}catch(EOFException eofe) {
+				System.out.println("End Of File Exception, no more data sent in this stream\n" + eofe);
+				//eofe.printStackTrace();
+				//break;
+				
+			}catch(IOException ioe) {
+				System.out.println("IO encountered while server running:\n" + ioe);
+				ioe.printStackTrace();
+			}
 			
-			if(key.length>0) {
-				//try {
+			/* decrypt symmetric key */
+			System.out.println("Encrypted symmetric key:" + encryptedAESKey);
+			realAESKey = rsa.decrypt(encryptedAESKey);
+			
+			/* use symmetric key to decrypt */
+			if(realAESKey.length>0) {
+				
 					/* string to contain the encrypted text */
 					byte[] encryptedByteArray = null;
 					
-					//while (true) {
-						
 						try {
 							
-							int length = inStream.readInt();
+							int length = dataBufferedNetInputStream.readInt();
 							System.out.println("length sent from client:" + length);
 							encryptedByteArray = new byte[length];
-							inStream.read(encryptedByteArray, 0, length);
+							dataBufferedNetInputStream.read(encryptedByteArray, 0, length);
 							System.out.println("length of encrypted array:" + encryptedByteArray.length);
 							
 							
@@ -106,28 +126,14 @@ public class Server {
 							System.out.println("IO encountered while server running:\n" + ioe);
 							ioe.printStackTrace();
 						}
-						
-					//}
 					
 					/* decrypt the text */
 					if(encryptedByteArray.length>0) {
 						
-						ByteBuffer byteBuffer = ByteBuffer.wrap(encryptedByteArray);
-						int ivLength = byteBuffer.getInt();
-						if(ivLength < 12 || ivLength >=16) {
-							throw new IllegalArgumentException("invalid iv length");
-						}
-						byte[] iv = new byte[ivLength];
-						byteBuffer.get(iv);
-						byte[] cipherText = new byte[byteBuffer.remaining()];
-						byteBuffer.get(cipherText);
-						
-						final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-						cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new GCMParameterSpec(128, iv));
-
-						byte[] plainText = cipher.doFinal(cipherText);
-						
-						String decryptedString = new String(plainText);
+						/* decrypt */
+						AES aes = new AES(realAESKey);
+						byte[] decrytpedByteArray = aes.decrypt(encryptedByteArray);						
+						String decryptedString = new String(decrytpedByteArray);
 						
 						System.out.println("decrypted String:" + decryptedString);
 						
@@ -140,24 +146,6 @@ public class Server {
 			System.out.println("End Of File Exception encountered while starting server:\n" + eofe);
 		}catch(IOException ioe) {
 			System.out.println("IO encountered while starting server:\n" + ioe);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidAlgorithmParameterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 }
